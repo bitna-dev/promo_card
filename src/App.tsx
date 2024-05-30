9adcf18e25fe74e0d6ed616a1a3dd50125d44511
Bitna <bitna@Bitnas-MacBook-Pro.local>
2024-05-30 10:12:03 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/shared/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Button size="large" full>
+        버튼
+      </Button>
+      <Button size="medium" color="error" weak>
+        버튼
+      </Button>
+    </>
+  )
 }
 
-export default App;
+export default App
